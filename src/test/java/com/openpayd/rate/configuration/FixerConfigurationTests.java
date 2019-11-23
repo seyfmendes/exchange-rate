dc2cdf3356ea81a49ba73c22bef25a9b438d565e
seyfmendes <seyfettin.mendes@etsadmins-MacBook-Pro.local>
2019-11-23 15:32:35 +0300

    added test application.yml. modified FixerConfigurationTests class

--- src/test/java/com/openpayd/rate/configuration/FixerConfigurationTests.java
+++ src/test/java/com/openpayd/rate/configuration/FixerConfigurationTests.java
@@ -1,16 +1,16 @@
 package com.openpayd.rate.configuration;
 
 
+import com.openpayd.rate.ExchangeRateApiApplication;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {FixerConfigurationTests.TestConfiguration.class})
+@SpringBootTest(classes = {ExchangeRateApiApplication.class})
 public class FixerConfigurationTests {
 
     @Autowired
@@ -21,9 +21,4 @@ public class FixerConfigurationTests {
         Assert.assertEquals(fixerConfiguration.getUrl(), "http://data.fixer.io/api/");
         Assert.assertEquals(fixerConfiguration.getApiKey(), "96bfcb20f940d0169074d3ca80a749ec");
     }
-
-    @EnableConfigurationProperties(FixerConfiguration.class)
-    public static class TestConfiguration {
-    }
-
 }
